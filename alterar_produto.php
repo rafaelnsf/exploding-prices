3676eab3be3bda6ed531cb002eb14b3d878aea32
rafaelnsf <rafaelnsf@hotmail.com>
2023-04-16 20:09:56 -0300

    add titles

--- alterar_produto.php
+++ alterar_produto.php
@@ -19,55 +19,56 @@ include('conexao.php');
 
 <body>
     <div class="conteudo">
-    <?php
-    include('menu.php');
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM produto WHERE id = '{$id}'";
-    $query = mysqli_query($con, $sql);
-    $arr = mysqli_fetch_array($query, MYSQLI_ASSOC);
-    ?>
-    <form action="alterar_produto_db.php" method="post">
-        <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <?php
+        include('menu.php');
+        $id = $_GET['id'];
+        $sql = "SELECT * FROM produto WHERE id = '{$id}'";
+        $query = mysqli_query($con, $sql);
+        $arr = mysqli_fetch_array($query, MYSQLI_ASSOC);
+        ?>
+        <h1>Alteração de Produtos</h1>
+        <form action="alterar_produto_db.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
 
-        <label>Código</label><br>
-        <?php echo $id; ?><br><br>
+            <label>Código</label><br>
+            <?php echo $id; ?><br><br>
 
-        <label for="nome">Nome</label><br>
-        <input type="text" name="nome" id="nome" value="<?php echo $arr['nome'] ?>" maxlength="50"><br><br>
+            <label for="nome">Nome</label><br>
+            <input type="text" name="nome" id="nome" value="<?php echo $arr['nome'] ?>" maxlength="50"><br><br>
 
-        <label for="descricao">Descrição</label><br>
-        <input type="text" name="descricao" id="descricao" value="<?php echo $arr['descricao'] ?>" maxlength="100"><br><br>
+            <label for="descricao">Descrição</label><br>
+            <input type="text" name="descricao" id="descricao" value="<?php echo $arr['descricao'] ?>" maxlength="100"><br><br>
 
-        <label for="preco">Preço</label><br>
-        <input type="text" name="preco" id="preco" value="<?php echo $arr['preco'] ?>" maxlength="100"><br><br>
+            <label for="preco">Preço</label><br>
+            <input type="text" name="preco" id="preco" value="<?php echo $arr['preco'] ?>" maxlength="100"><br><br>
 
-        <label for="estoque">Estoque</label><br>
-        <input type="text" name="estoque" id="estoque" value="<?php echo $arr['estoque'] ?>" maxlength="5"><br><br>
+            <label for="estoque">Estoque</label><br>
+            <input type="text" name="estoque" id="estoque" value="<?php echo $arr['estoque'] ?>" maxlength="5"><br><br>
 
-        <label for="id_categoria">Categoria</label><br>
-        <select name="id_categoria" id="id_categoria">
-            <?php
-            $sql = "SELECT id, nome FROM categoria WHERE status = 'A'";
-            $query = mysqli_query($con, $sql);
-            if ($query) {
-                while ($arr2 = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
-            ?>
-                    <option value="<?php echo $arr2['id']; ?>" <?php if ($arr['id_categoria'] == $arr2['id']) { ?> selected="selected" <?php } ?>><?php echo $arr2['nome']; ?></option>
-            <?php
+            <label for="id_categoria">Categoria</label><br>
+            <select name="id_categoria" id="id_categoria">
+                <?php
+                $sql = "SELECT id, nome FROM categoria WHERE status = 'A'";
+                $query = mysqli_query($con, $sql);
+                if ($query) {
+                    while ($arr2 = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+                ?>
+                        <option value="<?php echo $arr2['id']; ?>" <?php if ($arr['id_categoria'] == $arr2['id']) { ?> selected="selected" <?php } ?>><?php echo $arr2['nome']; ?></option>
+                <?php
+                    }
                 }
-            }
-            ?>
-        </select><br><br>
+                ?>
+            </select><br><br>
 
-        <label for="status">Status</label><br>
-        <select name="status" id="status">
-            <option value="A" <?php if ($arr['status'] == 'A') { ?>selected="selected" <?php } ?>>Ativo</option>
-            <option value="I" <?php if ($arr['status'] == 'I') { ?>selected="selected" <?php } ?>>Inativo</option>
-        </select><br><br>
+            <label for="status">Status</label><br>
+            <select name="status" id="status">
+                <option value="A" <?php if ($arr['status'] == 'A') { ?>selected="selected" <?php } ?>>Ativo</option>
+                <option value="I" <?php if ($arr['status'] == 'I') { ?>selected="selected" <?php } ?>>Inativo</option>
+            </select><br><br>
 
 
-        <button type="submit">Salvar</button>
-    </form>
+            <button type="submit">Salvar</button>
+        </form>
     </div>
 </body>
 
--- cadastrar_produto.php
+++ cadastrar_produto.php
@@ -16,46 +16,47 @@ include('conexao.php');
 
 <body>
     <div class="conteudo">
-    <?php
-    include('menu.php');
-    ?>
-    <form action="cadastrar_produto_db.php" method="post">
+        <?php
+        include('menu.php');
+        ?>
+        <h1>Cadastro de Produtos</h1>
+        <form action="cadastrar_produto_db.php" method="post">
 
-        <label for="nome">Nome do Produto</label><br>
-        <input type="text" name="nome" id="nome" maxlength="50"><br><br>
+            <label for="nome">Nome do Produto</label><br>
+            <input type="text" name="nome" id="nome" maxlength="50"><br><br>
 
-        <label for="descricao">Descrição</label><br>
-        <input type="text" name="descricao" id="descricao" maxlength="100"><br><br>
+            <label for="descricao">Descrição</label><br>
+            <input type="text" name="descricao" id="descricao" maxlength="100"><br><br>
 
-        <label for="preco">Preço</label><br>
-        <input type="number" name="preco" id="preco"><br><br>
+            <label for="preco">Preço</label><br>
+            <input type="number" name="preco" id="preco"><br><br>
 
-        <label for="estoque">Estoque</label><br>
-        <input type="number" name="estoque" id="estoque"><br><br>
+            <label for="estoque">Estoque</label><br>
+            <input type="number" name="estoque" id="estoque"><br><br>
 
-        <label for="id_categoria">Categoria</label><br>
-        <select name="id_categoria" id="id_categoria">
-            <?php
-            $sql = "SELECT id, nome FROM categoria WHERE status = 'A'";
-            $query = mysqli_query($con, $sql);
-            if ($query) {
-                while ($arr = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
-            ?>
-                    <option value="<?php echo $arr['id']; ?>"><?php echo $arr['nome']; ?></option>
-            <?php
+            <label for="id_categoria">Categoria</label><br>
+            <select name="id_categoria" id="id_categoria">
+                <?php
+                $sql = "SELECT id, nome FROM categoria WHERE status = 'A'";
+                $query = mysqli_query($con, $sql);
+                if ($query) {
+                    while ($arr = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+                ?>
+                        <option value="<?php echo $arr['id']; ?>"><?php echo $arr['nome']; ?></option>
+                <?php
+                    }
                 }
-            }
-            ?>
-        </select><br><br>
+                ?>
+            </select><br><br>
 
-        <label for="status">Status</label><br>
-        <select name="status" id="status">
-            <option value="A">Ativo</option>
-            <option value="I">Inativo</option>
-        </select><br><br>
+            <label for="status">Status</label><br>
+            <select name="status" id="status">
+                <option value="A">Ativo</option>
+                <option value="I">Inativo</option>
+            </select><br><br>
 
-        <button type="submit">Cadastrar</button>
-    </form>
+            <button type="submit">Cadastrar</button>
+        </form>
     </div>
 </body>
 
